c4b4c2937fe318eae0373ae093410dbf3caeaba2
马嘉骏 <m02655@outlook.com>
2022-08-18 16:19:57 +0800

    指定世界防爆

--- src/main/java/minecraft/yunzhong/api/McLogger.java
+++ src/main/java/minecraft/yunzhong/api/McLogger.java
@@ -1,100 +1,77 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
 package minecraft.yunzhong.api;
 
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.logging.*;
+import java.util.logging.FileHandler;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 public class McLogger {
     private static String file_name = "log";
 
+    public McLogger() {
+    }
+
     private static String getLogName() throws IOException {
         StringBuffer logPath = new StringBuffer();
-        System.setProperty("log", "plugins/YunZhong//");// 这里设置一个自定义的key作为存日志的目录
-        logPath.append(System.getProperty("log")); // 现将自定义的目录取出来
-        logPath.append("//"+file_name);
+        System.setProperty("log", "plugins/YunZhong//");
+        logPath.append(System.getProperty("log"));
+        logPath.append("//" + file_name);
         File file = new File(logPath.toString());
         if (!file.exists()) {
             file.mkdir();
         }
+
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-        logPath.append("//"+sdf.format(new Date())+".log");
+        logPath.append("//" + sdf.format(new Date()) + ".log");
         return logPath.toString();
-
     }
 
-
-    /**
-     * 配置Logger对象输出日志文件路径
-     * @param logger
-     * @throws SecurityException
-     * @throws IOException
-     */
     public static void setLogingProperties(Logger logger) throws SecurityException, IOException {
-        setLogingProperties(logger,Level.ALL);
+        setLogingProperties(logger, Level.ALL);
     }
 
-
-
-    /**
-     * 配置Logger对象输出日志文件路径
-     * @param logger
-     * @param level 在日志文件中输出level级别以上的信息
-     * @throws SecurityException
-     * @throws IOException
-     */
-    public static void setLogingProperties(Logger logger,Level level) {
-        FileHandler fh;
+    public static void setLogingProperties(Logger logger, Level level) {
         try {
-            fh = new FileHandler(getLogName(),true);
-            logger.addHandler(fh);//日志输出文件
-            //logger.setLevel(level);
-            fh.setFormatter(new SimpleFormatter());//输出格式
-            //logger.addHandler(new ConsoleHandler());//输出到控制台
-        } catch (SecurityException e) {
-            logger.log(Level.SEVERE, "安全性错误", e);
-        } catch (IOException e) {
-            logger.log(Level.SEVERE,"读取文件日志错误", e);
+            FileHandler fh = new FileHandler(getLogName(), true);
+            logger.addHandler(fh);
+            fh.setFormatter(new SimpleFormatter());
+        } catch (SecurityException var4) {
+            logger.log(Level.SEVERE, "安全性错误", var4);
+        } catch (IOException var5) {
+            logger.log(Level.SEVERE, "读取文件日志错误", var5);
         }
 
     }
-
 
     public static void info(String info) {
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        new SimpleDateFormat("yyyy-MM-dd");
         Logger logger = Logger.getLogger("sgg");
+
         try {
-            McLogger.setLogingProperties(logger);
+            setLogingProperties(logger);
             logger.log(Level.INFO, info);
-            for (Handler h:logger.getHandlers()){
+            Handler[] var3 = logger.getHandlers();
+            int var4 = var3.length;
+
+            for(int var5 = 0; var5 < var4; ++var5) {
+                Handler h = var3[var5];
                 h.close();
             }
-        } catch (SecurityException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+        } catch (SecurityException var7) {
+            var7.printStackTrace();
+        } catch (IOException var8) {
+            var8.printStackTrace();
         }
+
     }
-
-
-//    public static void main(String [] args) {
-//        Logger logger = Logger.getLogger("sgg");
-//        try {
-//            FileHandler fh = null;
-//            McLogger.setLogingProperties(fh,logger);
-//            logger.log(Level.INFO, "某某玩家防止了一个刷怪笼");
-//            logger.log(Level.INFO, "[Jetren]说：大家还好吗？");
-//
-//
-//        } catch (SecurityException e) {
-//            // TODO Auto-generated catch block
-//            e.printStackTrace();
-//        } catch (IOException e) {
-//            // TODO Auto-generated catch block
-//            e.printStackTrace();
-//        }
-//    }
 }
--- src/main/java/minecraft/yunzhong/api/Profile.java
+++ src/main/java/minecraft/yunzhong/api/Profile.java
@@ -1,85 +1,104 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
 package minecraft.yunzhong.api;
 
-import org.bukkit.Bukkit;
-
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Properties;
 
 public class Profile {
-	public final static void ReloadProfile() {
+	public Profile() {
+	}
+
+	public static final void ReloadProfile() {
 		FileInputStream fis = null;
-		  Properties properties = new Properties();
-		  File file = new File("plugins/YunZhong/YunZhong.properties");
-		  File folder = new File("plugins/YunZhong");
-		  if(!folder.exists()){
-			  folder.mkdir();
-		  }
-		  if(!file.exists()){//????????????
-			  try {
+		Properties properties = new Properties();
+		File file = new File("plugins/YunZhong/YunZhong.properties");
+		File folder = new File("plugins/YunZhong");
+		if (!folder.exists()) {
+			folder.mkdir();
+		}
+
+		if (!file.exists()) {
+			try {
 				file.createNewFile();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}//???????
-		  }
-		   try {
+			} catch (IOException var11) {
+				var11.printStackTrace();
+			}
+		}
+
+		try {
 			fis = new FileInputStream(file);
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		} catch (FileNotFoundException var10) {
+			var10.printStackTrace();
 		}
-		  try {
+
+		try {
 			properties.load(fis);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		} catch (IOException var9) {
+			var9.printStackTrace();
 		}
-		  if(properties.getProperty("BossName")==null) {
-			  properties.setProperty("BossName","Jetren");
-		  }		
-		  if(properties.getProperty("F")==null) {
-			  properties.setProperty("F","/help");
-			  properties.setProperty("F_state","true");
-		  }	
-		  if(properties.getProperty("Anti_cheating_fall_state")==null) {
-			  properties.setProperty("Anti_cheating_fall_state","true");
-		  }	
-		  if(properties.getProperty("public_off_fly")==null) {
-			  properties.setProperty("public_off_fly","true");
-		  }	
-		  if(properties.getProperty("sleep_morning")==null) {
-			  properties.setProperty("sleep_morning","true");
-		  }
-		  FileWriter fWriter = null;
+
+		if (properties.getProperty("BossName") == null) {
+			properties.setProperty("BossName", "Jetren");
+		}
+
+		if (properties.getProperty("F") == null) {
+			properties.setProperty("F", "/help");
+			properties.setProperty("F_state", "true");
+		}
+
+		if (properties.getProperty("Anti_cheating_fall_state") == null) {
+			properties.setProperty("Anti_cheating_fall_state", "true");
+		}
+
+		if (properties.getProperty("public_off_fly") == null) {
+			properties.setProperty("public_off_fly", "true");
+		}
+
+		if (properties.getProperty("sleep_morning") == null) {
+			properties.setProperty("sleep_morning", "true");
+		}
+
+		FileWriter fWriter = null;
+
 		try {
 			fWriter = new FileWriter("plugins/yzzm/yzzm.properties");
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		} catch (IOException var8) {
+			var8.printStackTrace();
 		}
-		  try {
+
+		try {
 			properties.store(fWriter, "yzzm");
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		} catch (IOException var7) {
+			var7.printStackTrace();
 		}
-		  try {
+
+		try {
 			fis.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		} catch (IOException var6) {
+			var6.printStackTrace();
 		}
+
 	}
-	public static  final FileInputStream initialization() {
-		  FileInputStream fis = null;
-		  Properties properties = new Properties();
-		  File file = new File("plugins/yzzm/yzzm.properties");
-		  try {
+
+	public static final FileInputStream initialization() {
+		FileInputStream fis = null;
+		new Properties();
+		File file = new File("plugins/yzzm/yzzm.properties");
+
+		try {
 			fis = new FileInputStream(file);
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		} catch (FileNotFoundException var4) {
+			var4.printStackTrace();
 		}
-		 return fis;
+
+		return fis;
 	}
 }
